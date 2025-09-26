232f33b83f8cc213d1dc041acbc95ae03497bb90
Grace Wei <hhtet263510@gmail.com>
2025-09-26 14:56:11 +0700

    es lint disable

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,4 @@
-
 /** @type {import('next').NextConfig} */
-const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5002';
-
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${BACKEND_URL}/:path*`, 
-      },
-    ];
-  },
-  eslint: { ignoreDuringBuilds: true }, 
+  eslint: { ignoreDuringBuilds: true },
 };
