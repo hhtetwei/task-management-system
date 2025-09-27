a8a1164a4eb2e2c010fd5468c67acdfc0ab252c4
Grace Wei <hhtet263510@gmail.com>
2025-09-27 16:01:47 +0700

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';  
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
